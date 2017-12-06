b763f95c15767cea4b36960a2d2ec6711384edc7
Adam Cigánek <adam.ciganek@gmail.com>
2017-12-06 16:16:20 +0100

    chore/meta: update Cargo.toml and add COPYING and CONTRIBUTOR

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2016 MaidSafe.net limited.
+// Copyright 2017 MaidSafe.net limited.
 //
 // This SAFE Network Software is licensed to you under (1) the MaidSafe.net Commercial License,
 // version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
@@ -14,6 +14,28 @@
 //
 // Please review the Licences for the specific language governing permissions and limitations
 // relating to use of the SAFE Network Software.
+
+//! ifaddrs
+#![doc(html_logo_url = "https://raw.githubusercontent.com/maidsafe/QA/master/Images/
+maidsafe_logo.png",
+      html_favicon_url = "http://maidsafe.net/img/favicon.ico",
+      html_root_url = "http://maidsafe.github.io/ifaddrs")]
+// For explanation of lint checks, run `rustc -W help` or see
+// https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md
+#![forbid(exceeding_bitshifts, mutable_transmutes, no_mangle_const_items, unknown_crate_types,
+         warnings)]
+#![deny(bad_style, deprecated, improper_ctypes, missing_docs, non_shorthand_field_patterns,
+       overflowing_literals, plugin_as_library, private_no_mangle_fns, private_no_mangle_statics,
+       stable_features, unconditional_recursion, unknown_lints, unsafe_code, unused,
+       unused_allocation, unused_attributes, unused_comparisons, unused_features, unused_parens,
+       while_true)]
+#![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
+       unused_qualifications, unused_results)]
+#![allow(box_pointers, missing_copy_implementations, missing_debug_implementations,
+         variant_size_differences)]
+#![cfg_attr(feature = "cargo-clippy",
+           deny(clippy, unicode_not_nfc, wrong_pub_self_convention, option_unwrap_used))]
+#![cfg_attr(feature = "cargo-clippy", allow(use_debug, too_many_arguments))]
 
 use std::io;
 use std::net::{Ipv4Addr, Ipv6Addr};
